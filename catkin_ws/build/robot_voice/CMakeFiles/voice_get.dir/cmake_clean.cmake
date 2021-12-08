346693658bfc75a5a75163b49eb08df333d4831b
Allen0497 <93314905+Allen0497@users.noreply.github.com>
2021-12-08 19:27:59 +0000

    Add files via upload

---
+++ catkin_ws/build/robot_voice/CMakeFiles/voice_get.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/u/catkin_ws/devel/lib/robot_voice/voice_get"
+  "/home/u/catkin_ws/devel/lib/robot_voice/voice_get.pdb"
+  "CMakeFiles/voice_get.dir/src/linuxrec.c.o"
+  "CMakeFiles/voice_get.dir/src/speech_recognizer.c.o"
+  "CMakeFiles/voice_get.dir/src/voice_get.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/voice_get.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
