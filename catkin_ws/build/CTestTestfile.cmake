ff6b3e8713482702131e233e3346a8a02d7918c3
Allen0497 <93314905+Allen0497@users.noreply.github.com>
2021-12-08 19:37:05 +0000

    Add files via upload

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/u/catkin_ws/src
+# Build directory: /home/u/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_voice")
+subdirs("final_project")
+subdirs("robot_sim")
